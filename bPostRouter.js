d46749d5769b7e60c760d83aefd53e0fd64fab60
giri68 <giri.pdt@gmail.com>
2017-09-07 13:05:48 -0700

    Added create functionality to the blogpost

--- bPostRouter.js
+++ bPostRouter.js
@@ -1,12 +1,21 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
 const router = express.Router();
 const {BlogPosts} = require('./models');
 
 router.get('/', (req, res) => {
   console.log('running get in router module');
   res.json(BlogPosts.get());
+});
+
+router.post('/', jsonParser, (req, res) => {
+  console.log('running posts');
+  const item = BlogPosts.create(req.body.title, req.body.content, req.body.author);
+  console.log("running post" + item);
+  res.status(201).json(item);
 });
 
 
